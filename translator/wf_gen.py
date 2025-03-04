4778508c5f114d36fd7b7c59a6d4e7984096bbc4
Jeff Wong <42687471+jeff-yc-wong@users.noreply.github.com>
2025-03-03 16:54:30 -1000

    changes to benchmark generation

--- translator/wf_gen.py
+++ translator/wf_gen.py
@@ -1,19 +1,63 @@
 import pathlib
-
-from wfcommons import BlastRecipe, MontageRecipe, SoykbRecipe
-from wfcommons.wfbench import WorkflowBenchmark, DaskTranslator, CWLTranslator, ParslTranslator
-from wfcommons.wfinstances import Instance
-from wfcommons import WorkflowGenerator
-
-
 import argparse
-import json
 import copy
+
+from wfcommons import BlastRecipe, MontageRecipe, SoykbRecipe, EpigenomicsRecipe
+from wfcommons import GenomeRecipe, BwaRecipe, CyclesRecipe, SrasearchRecipe
+from wfcommons.wfbench import WorkflowBenchmark
+from wfcommons.wfinstances import Instance
+
+
+recipes = {
+    "makeflow-blast-medium": BlastRecipe,
+    "1000genome-20200402T023420Z-0": GenomeRecipe,
+    "makeflow-bwa-large": BwaRecipe,
+    "Cycles-20200413T220633Z-0": CyclesRecipe,
+    "genome-dax-0": EpigenomicsRecipe,
+    "montage": MontageRecipe,
+    "soykb-0": SoykbRecipe,
+    "workflow-test": SrasearchRecipe
+}
+
+def write_benchmark(workflow_path: str):
+    # create a workflow benchmark from a synthetic workflow (workflow used in the fgcs paper)
+    workflow_instance = Instance(workflow_path)
+    workflow = workflow_instance.workflow
+    cpu_work = {}
+    for task in workflow.tasks.values():
+        task.category = task.task_id
+        cpu_work[task.category] = task.runtime * task.avg_cpu * 100
+        task.avg_cpu = None
+        task.memory = None
+
+    if workflow.name not in recipes:
+        print(f"Recipe for {workflow.name} not found.")
+        return
+    benchmark = WorkflowBenchmark(recipe=recipes[workflow.name], num_tasks=len(workflow.tasks))
+    benchmark.workflow = copy.deepcopy(workflow)
+    output_path = pathlib.Path(f"./benchmarks/{workflow.name}")
+    path = benchmark.create_benchmark(output_path, percent_cpu=1.0, cpu_work=cpu_work, regenerate=False)
+
+    for key, task in benchmark.workflow.tasks.items():
+        task.output_files = workflow.tasks[key].output_files
+        task.input_files = workflow.tasks[key].input_files
+
+        output_files = {file.file_id: file.size for file in task.output_files}
+        input_files = {file.file_id: file.size for file in task.input_files}
+
+        task.args.append(f"--output-files {output_files}")
+        task.args.append(f"--input-files {input_files}")
+
+    benchmark._rename_files_to_wfbench_format()
+
+    benchmark.workflow.write_json(path)
+
 
 def main():
     parser = argparse.ArgumentParser(description="Process a workflow JSON file.")
     parser.add_argument("--workflow", type=str, help="Path to the workflow JSON file.")
     parser.add_argument("--test", action="store_true", help="Create a test benchmark")
+    parser.add_argument("--all", action="store_true", help="Create benchmarks for all workflows")
     
     args = parser.parse_args()
 
@@ -57,12 +101,14 @@ def main():
         # output_path = pathlib.Path(f"./benchmarks/{workflow_instance.name}")
         # path = benchmark.create_benchmark_from_synthetic_workflow(output_path, workflow_instance.workflow)
     if args.workflow:
-        # create a workflow benchmark from a synthetic workflow (workflow used in the fgcs paper)
-        workflow_instance = Instance(args.workflow)
-        benchmark = WorkflowBenchmark(recipe=SoykbRecipe, num_tasks=156)
-        output_path = pathlib.Path(f"./benchmarks/{workflow_instance.name}")
-        path = benchmark.create_benchmark_from_synthetic_workflow(output_path, workflow_instance.workflow)
-    
+        write_benchmark(args.workflow)
+        
+    if args.all:
+        all_path = pathlib.Path("./workflows")
+
+        for file in all_path.iterdir():
+            if file.suffix == ".json":
+                write_benchmark(file)
 
 if __name__ == "__main__":
     main()
