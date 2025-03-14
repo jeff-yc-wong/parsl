990fdc3c3499f614e061b76bdbd7ff7af74c83d0
Jeff Wong <42687471+jeff-yc-wong@users.noreply.github.com>
2025-03-13 16:53:56 -1000

    added a user instead of running as root in docker container

--- docker/entry.sh
+++ docker/entry.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-/usr/sbin/sshd &
+sudo /usr/sbin/sshd &
 
 if [[ $# -eq 1 && $1 =~ ^-?[0-9]+$  ]]; then
-  echo "CPU_SPEED=$1" >> /etc/environment
+  echo "CPU_SPEED=$1" | sudo tee -a /etc/environment
 else
-  echo "CPU_SPEED=1" >> /etc/environment
+  echo "CPU_SPEED=1" | sudo tee -a /etc/environment
 fi
 
 tail -f /dev/null
