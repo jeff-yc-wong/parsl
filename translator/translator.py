20bf7ab14cef6947862cbc7f30e0d7f8b4ae9aee
Jeff Wong <42687471+jeff-yc-wong@users.noreply.github.com>
2025-03-26 13:22:04 -1000

    bug--

--- translator/translator.py
+++ translator/translator.py
@@ -289,7 +289,6 @@ def main():
                 for wf_file in wf.iterdir():
                     if wf_file.name.endswith(".json"):
                         translate_workflow(wf_file, outdir_path)
-    else
 
     return 0
 
