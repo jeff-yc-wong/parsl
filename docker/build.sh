28ee493e8a61087ffc4c83722d83d117bc40dcd5
Jeff Wong <wongy@hawaii.edu>
2025-04-10 15:34:52 -1000

    set the uid in the container to be the same as the user starting the container (read/write permissions)

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 if [ $# -eq 1 ]; then
-  docker build --target builder -t parsl-worker --build-arg CACHEBUST=$(date +%s) .
+	docker build --target builder -t parsl-worker --build-arg CACHEBUST=$(date +%s) --build-arg USER_UID=$(id -u) .
 else
-  docker build --target builder -t parsl-worker .
+	docker build --target builder -t parsl-worker --build-arg USER_UID=$(id -u) .
 fi
